7cda6a7e81374fbcafb179e0a19896ac146f3e3e
Sebastian Chłoń <Sebastiaan.chlon@gmail.com>
2022-08-22 22:53:18 +0200

    Post and Get

---
+++ WebAPI/Controllers/PostsController.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly IPostService _postService;
+
+        public PostsController(IPostService postServices)
+        {
+            _postService = postServices;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var posts = _postService.GetAllPosts();
+            return Ok(posts);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var post = _postService.GetById(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+    }
+}
---
+++ WebAPI/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interfaces;
+using Application.Mappings;
+using Application.Services;
+using Domain.Interfaces;
+using Infrascructure.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.OpenApi.Models;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IPostRepository, PostRepository>();
+            services.AddScoped<IPostService, PostService>();
+            services.AddSingleton(AutoMapperConfig.Initialize());
+
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
+            });
+        }
+    }
+}
